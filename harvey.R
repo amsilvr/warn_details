4a734fc1d2ce4f50d8c2aa7ecc126e4717374b85
amsilvr <silverman.aaron@gmail.com>
2018-01-02 17:25:08 -0500

    removed unused files

--- harvey.R
+++
@@ -1,9 +0,0 @@
-harvey_events <- msg2 %>%
-    filter(rec_time >= mdy("08-23-2017"), ) %>%
-    filter(rec_time <= mdy("9/1/17")) %>%
-    filter( grepl("TX|LA", areas) == TRUE)
-
-harvey_events %>% group_by(type) %>% summarize(n())
-
-select(harvey_events, rec_time, wea) %>% arrange(rec_time) %>% View()
-
